e50621e52b781fcedd2c09aaecb1c560875d5d69
Shahriar Mahmud Tanim <157669438+tanimweb1@users.noreply.github.com>
2026-01-08 01:13:37 +0600

    Add files via upload

---
+++ Day-22.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+
+// First two maximum numbers in array
+int numbers [] = {14,12};
+int lengthOfArray = sizeof(numbers)/sizeof(numbers[0]);
+
+if(lengthOfArray<2){
+    printf("It's need at least 2 numbers");
+}
+
+int first,second;
+if(numbers[1]>numbers[0]){
+    first = numbers[1];
+    second = numbers[0];
+
+}else{
+    first = numbers[0];
+    second = numbers[1];
+}
+
+printf("Fast largest number is %d\n",first);
+printf("Second largest number is %d\n",second);
+
+
+
+
+    return 0;
+}
